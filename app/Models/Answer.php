ded1bf02c804725e040ed0e05a72b68df5f24a42
arsitanrfzh <arsita.nurfauziah@gmail.com>
2025-11-11 19:17:05 +0700

    Mendefinisikan semua relasi Eloquent

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
